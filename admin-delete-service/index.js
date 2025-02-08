259a6e3e59240b1107fbeb5a060920ba2ef465e1
lxgonzalez <luisgx15@gmail.com>
2025-02-08 18:26:17 -0500

    feat: :sparkles: Correction

--- admin-delete-service/index.js
+++ admin-delete-service/index.js
@@ -3,7 +3,7 @@ const Admin = require('./model/admin');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.json()); /
+app.use(express.json()); 
 
 app.get('/', (req, res) => {
     res.status(200).send('Delete Admin Service is running ...');
